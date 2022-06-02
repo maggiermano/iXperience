512b0b86127292d310d223b985fb723243e7dcde
maggiermano <70844070+maggiermano@users.noreply.github.com>
2022-06-02 18:52:03 +0200

    Create script.js

---
+++ day_4/DOM_Example/script.js
@@ -0,0 +1,32 @@
+//console.log(document);
+
+/* const element = document.getElementById('yah');
+
+//const anElem = element.parentElement.parentElement.children[0].children.[2];
+// element.classList.add('p-5');
+
+// const cells = document.getElementsByName('second');
+
+const containers = document.getElementsByClassName('container');
+//anElem.style="background-color: red;";
+
+const newElem = document.createElement('div');
+newElem.innerHTML = "I am a new DIV"
+
+const container = containers[0];
+
+// container.setAttribute('label', 'howdy');
+container.Attribute('label', 'howdy');
+
+container.appendChild(newElem);
+
+//console.log(anElem); */
+
+const form = document.getElementById("form");
+const nameInput = document.getElementById("name-input");
+const surnameInput = document.getElementById("surname-input");
+const button = document.getElementById("button");
+
+form.addEventListener('mouseenter', () => {
+    
+});
