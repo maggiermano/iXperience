c6240f2229f78a62bd5cc711b57433a561cca705
maggiermano <70844070+maggiermano@users.noreply.github.com>
2022-06-03 18:52:44 +0200

    Create script.js

---
+++ day_5/oop_example/script.js
@@ -0,0 +1,35 @@
+class Person{
+    constructor(firstName, lastName, dob) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.dob = dob;
+    }
+
+    getFullName(){
+        return this.firstName + " " + this.lastName;
+    }   
+}
+
+
+class Customer extends Person {
+    constructor(firstName, lastName, dob, phone, membership){
+        // when we instantiate a customer we want to call the person constructor
+        super(firstName, lastName, dob);
+ 
+        this.phone = phone;
+        this.membership = membership;
+    }
+
+    static getMembershipCost(){
+        return 500;
+    }
+}
+const john = new Customer(
+    'John', 
+    'Doe', 
+    '23 / 11 / 1993', 
+    '011 425 6536', 
+    'Premium'
+);
+
+console.log(john);
