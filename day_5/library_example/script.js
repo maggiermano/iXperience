d04dad62d9ebafeeb7cd5a4696c8e16853e964f1
maggiermano <70844070+maggiermano@users.noreply.github.com>
2022-06-03 20:18:43 +0200

    Update script.js

--- day_5/library_example/script.js
+++ day_5/library_example/script.js
@@ -1,6 +1,6 @@
 
 class Book {
-    constructor(DataTransferItemList, author, isbn) {
+    constructor(title, author, isbn) {
         this.title = title;
         this.author = author;
         this.isbn = isbn;
@@ -8,7 +8,7 @@ class Book {
 }
 
 class UserInterface {
-
+  
     constructor(){
         this.form = document.getElementById('form');
 
@@ -16,17 +16,19 @@ class UserInterface {
         this.author = document.getElementById('author-input');
         this.isbn = document.getElementById('isbn-input');
 
-        this.table = document.getElementById('table-body');
+        this.tableBody = document.getElementById('table-body');
 
-        this.form.addEventListener('submit', (event) => {
-            this.onFormSubmitted(event);
-        });
+        this.form.addEventListener('submit', (e) =>
+            this.onFormSubmitted(e));
 
         this.books = [];
+
+        this.loadBooksFromLocalStorage();
+        this.renderTableBody();
     }
 
-    onFormSubmitted(event) {
-        event.preventDefault();
+    onFormSubmitted(e) {
+        e.preventDefault();
 
         const book = new Book(
             this.title.value,
@@ -35,11 +37,90 @@ class UserInterface {
         );
 
         this.books.push(book);
+        this.saveBooksToLocalStorage();
+        this.renderTableBody();
+
+        this.title.value = '';
+        this.author.value = '';
+        this.isbn.value = '';
     }
 
     renderTableBody() {
-        
+        this.tableBody.innerHTML = '';
+
+        for (let i = 0; i < this.books.length; i++) {
+            const book = this.books[i];
+            
+            const tableRow = this.generateBookRow(book);
+            this.tableBody.appendChild(tableRow);
+        }
+    }
+
+    /*
+    <tr>
+        <td></td> // title
+        <td></td> // author
+        <td></td> // isbn
+        <td></td> // actions
+    </tr>
+    */
+    generateBookRow(book) {
+        const tr = document.createElement('tr');
+
+        const cellTitle = document.createElement('td');
+        const cellAuthor = document.createElement('td');
+        const cellISBN = document.createElement('td');
+        const cellActions = document.createElement('td');
+
+        cellTitle.innerHTML = book.title;
+        cellAuthor.innerHTML = book.author;
+        cellISBN.innerHTML = book.isbn;
+
+        const removeButton = this.generateRemoveButton(book);
+        cellActions.appendChild(removeButton);
+
+        tr.appendChild(cellTitle);
+        tr.appendChild(cellAuthor);
+        tr.appendChild(cellISBN);
+        tr.appendChild(cellActions);
+
+        return tr;
+    }
+
+    generateRemoveButton(book) {
+        const button = document.createElement('button');
+
+        button.setAttribute('class', 'btn btn-sm btn-danger');
+        button.innerHTML = 'X';
+        button.addEventListener('click', () => 
+            this.onRemoveBookClicked(book)
+        );
+
+        return button;
+    }
+
+    onRemoveBookClicked(book) {
+
+        this.books = this.books.filter((x) => {
+            return book.isbn !== x.isbn;
+        });
+
+        this.saveBooksToLocalStorage();
+        this.renderTableBody();
+    }
+
+    saveBooksToLocalStorage() {
+        const json = JSON.stringify(this.books);
+        localStorage.setItem('books', json);
+    }
+
+    loadBooksFromLocalStorage() {
+        const json = localStorage.getItem('books');
+        if (json) {
+          const bookArr = JSON.parse(json);
+          this.books = bookArr.map(x => Book.fromJSON(x));
+        }
     }
 }
 
-new UserInterface();
+const ui = new UserInterface();
